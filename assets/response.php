77c1d2cab09b93e200c9a1b248e3258fe4bedd53
Lisandro Zapata <85231001+LisoProgrammer@users.noreply.github.com>
2022-04-13 19:30:42 -0500

    Update response.php

--- assets/response.php
+++ assets/response.php
@@ -1,5 +1,13 @@
 <?php
-header("Access-Control-Allow-Origin: *");
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+header("Allow: GET, POST, OPTIONS, PUT, DELETE");
+$method = $_SERVER['REQUEST_METHOD'];
+if($method == "OPTIONS") {
+    die();
+}
+
 error_reporting(0);
 if(isset($_GET)){
     $name = $_GET["name"];
