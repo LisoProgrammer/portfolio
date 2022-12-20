abf2e868be2e7f8af0e2ed760e8a9df98506d111
Lisandro Zapata <85231001+LisoProgrammer@users.noreply.github.com>
2022-12-20 13:00:39 -0500

    Entries

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 const observer = new IntersectionObserver(an, {
-    root: null,
-    rootMargin: "0px 0px 40px 0px",
+    root: document.getElementsByClassName("c")[0],
+    rootMargin: "0px",
     threshold: 1.0
 })
 
