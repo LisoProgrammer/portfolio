32af4f42c3b1de790723838f29545df8a5b8795b
Lisandro Zapata <85231001+LisoProgrammer@users.noreply.github.com>
2023-06-21 01:55:23 +0000

    ok

--- statics/load_st.js
+++ statics/load_st.js
@@ -3,34 +3,93 @@
 let fecha_st = new Date();
 let dateString = fecha_st.toLocaleDateString();
 let day_st = fecha_st.getDay();
+//Cambiar a false una vez se haga el test
+let is_new_visit = false;
 //JSON
 var jsonParse;
+let json_loaded;
+
 let http_request = new XMLHttpRequest();
-http_request.open('GET','statics.json');
-http_request.onreadystatechange = function(){
-    if(http_request.readyState == 4 && http_request.status == 200){
+http_request.open('GET', 'statics.json');
+http_request.onreadystatechange = function () {
+    if (http_request.readyState == 4 && http_request.status == 200) {
         jsonParse = JSON.parse(http_request.responseText);
         console.log(jsonParse);
-        for(let fecha in jsonParse){
-            if(fecha==dateString){
-                console.log("Existe");
-                jsonParse[fecha]["visitantes"] += 1;
-            }else{
-                jsonParse[dateString] = 0;
-            }
-        }
-        console.log(jsonParse[dateString]);
+        json_loaded = load_visit(jsonParse);
+        localStorage.setItem("reg",JSON.stringify(json_loaded));
     }
 }
 http_request.send();
 
-//Si no existe el dia registrado, es la primera visita
-if(localStorage.getItem("day_st") == null){
-   localStorage.setItem("day_st",day_st);
+let segs = 0;
+let mins = 0;
+setInterval(()=>{
+    segs++;
+    if(segs==60){
+        mins++;
+        segs = 0;
+    }
+},1000)
+function load_visit(json) {
+    if (is_new_visit) {
+        for (let fecha in json) {
+            if (fecha == dateString) {
+                console.log("Existe");
+                json[fecha]["visitantes"] += 1;
+                json[fecha]["devices"].push(device());
+                window.addEventListener("beforeunload",()=>{
+                    json["tiempos"].push(mins);
+                    let sum = 0;
+                    for(let time in json["tiempos"]){
+                        sum+=time;
+                    }
+                    json["tiempo_prom"] = sum/json["tiempo_prom"];
+                })
+                break;
+            } else {
+                //json[dateString] = 0;
+                break;
+            }
+        }
+        console.log("El usuario ingreso otro día más.");
+        console.log(json[dateString]);
+        return json;
+    } else {
+        console.log("El usuario entró nuevamente hoy.");
+    }
 
 }
-if(localStorage.getItem("dateString") == null){
-    localStorage.setItem("dateString",dateString);
+function device() {
+    var userAgent = navigator.userAgent;
+    var deviceBrand = "Desconocido";
+
+    if (userAgent.match(/iPhone|iPod|iPad/i)) {
+        deviceBrand = "Apple";
+    } else if (userAgent.match(/Android/i)) {
+        deviceBrand = "Android";
+    } else if (userAgent.match(/Windows Phone/i)) {
+        deviceBrand = "Windows Phone";
+    } else if (userAgent.match(/BlackBerry/i)) {
+        deviceBrand = "BlackBerry";
+    } else if (userAgent.match(/Windows/i)) {
+        deviceBrand = "Windows";
+    } else if (userAgent.match(/Macintosh|Mac OS X/i)) {
+        deviceBrand = "Mac";
+    } else if (userAgent.match(/Linux/i)) {
+        deviceBrand = "Linux";
+    }
+
+    console.log("Marca del dispositivo: " + deviceBrand);
+    return deviceBrand;
+}
+
+//Si no existe el dia registrado, es la primera visita
+if (localStorage.getItem("day_st") == null) {
+    localStorage.setItem("day_st", day_st);
+
+}
+if (localStorage.getItem("dateString") == null) {
+    localStorage.setItem("dateString", dateString);
 }
 /*//Si intentan cambiar la config
 window.localStorage.addEventListener("storage",()=>{
@@ -38,9 +97,9 @@ window.localStorage.addEventListener("storage",()=>{
 })*/
 
 //Cuando se cargue el documento
-document.addEventListener("DOMContentLoaded",()=>{
-    //Si el día registrado es diferente al arrojado
-    if(fecha_st.getDay()!=localStorage.getItem("day_st")){
-        console.log("exi");
+document.addEventListener("DOMContentLoaded", () => {
+    //Si la fecha registrada es diferente a la arrojada
+    if (dateString != localStorage.getItem("dateString")) {
+        is_new_visit = true;
     }
 })
