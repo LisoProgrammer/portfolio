b503e3f95fe8a778a080d4be22caa9ef63afe3d7
Lisandro Zapata <85231001+LisoProgrammer@users.noreply.github.com>
2023-07-11 21:09:04 +0000

    ok

--- assets/delcaribe/js/index.js
+++ assets/delcaribe/js/index.js
@@ -4,10 +4,18 @@ http.open('GET','./menu/menu.json');
 window.onload = () =>{
     cloader.remove()
 }
+let state_ord_menu = 0;
+//template ORDENAR
+let capa_pri = document.getElementById("capa_pri")
+let capa_sec = document.getElementById("capa_sec")
+let name_prod_html = document.getElementById("name_prod")
+let precio_prod_html = document.getElementById("precio")
+let total_prod_html = document.getElementById("total")
+let input_cantidad = document.getElementById("cant")
 http.onreadystatechange = () => {
     if (http.readyState == 4 && http.status == 200){
         menujson = JSON.parse(http.responseText);
-        console.log(menujson);
+        //console.log(menujson);
         
         for(let ent in menujson["entradas"]){
             console.log(ent)
@@ -28,10 +36,12 @@ http.onreadystatechange = () => {
             console.log(ent)
             //console.log(menujson["entrada"][ent])
             generarTarjeta(menujson["bebidas"],ent,"beb")
-        }    
+        }
+            
     }
 }
 http.send()
+
 function generarTarjeta(cat, item, id_container) {
     //El json debe tener un array de objetos en cada categoria
     DIV_TYPE = document.getElementById(id_container)
@@ -81,5 +91,46 @@ function generarTarjeta(cat, item, id_container) {
     button_order.innerText = "ORDENAR";
     divtar.appendChild(div_cbutton)
     div_cbutton.appendChild(button_order)
+    button_order.addEventListener("click",()=>{
+        console.log("Hola, me haz presionado. Mi id es "+button_order.id);
+        for(let it in cat){
+            //console.log(cat[it])
+            //si el codigo del producto es igual al id del boton generado
+            if(cat[it]["code"]==button_order.id){
+                //Mostrar el objeto del producto
+                console.log(cat[it])
+                name_prod_html.innerText = cat[it]["nombre"]
+                precio_prod_html.innerText = cat[it]["precio"]
+                input_cantidad.addEventListener("input",()=>{
+                    if(input_cantidad.value.length == 0){
+                        input_cantidad.value = 0;
+                        //console.log(input_cantidad.value.length)
+                    }
+                    total_prod_html.innerText = parseInt(input_cantidad.value) * cat[it]["precio"]
+                    
+    
+                })
+                function total(){
+                    
+                }
+                capa_pri.className = "capa_pri capa_pri_act"
+                capa_sec.className = "capa_sec capa_sec_act"
+                
+            }
+        }
+    }
+    )
 }
+let button_closer = document.getElementById("closer_orden")
+
+document.addEventListener("click", function(e) {
+    let em = e.target;
+    if (em.id == "capa_pri" || em.id == "closer_orden") {
+        //cierra la capa
+        capa_pri.className = "capa_pri";
+        capa_sec.className = "capa_sec";
+        input_cantidad.value = 0;
+    }
+})
+
 
