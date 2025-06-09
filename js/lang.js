720435dce6256e1ea68a1e8ed6cee9099ff355f5
Lisandro Zapata <zlisandro5@gmail.com>
2025-06-09 13:01:29 -0500

    Se obtiene el idioma almacenado en localStorage, sino se encuentra se crea uno

--- js/lang.js
+++ js/lang.js
@@ -2,23 +2,27 @@ async function loadLanguage(lang) {
   try {
     const response = await fetch(`./lang/${lang}.json`);
     const translations = await response.json();
-
+    console.log(translations);
     document.querySelectorAll("[data-i18n]").forEach((el) => {
       const key = el.getAttribute("data-i18n");
       if (translations[key]) {
         el.textContent = translations[key];
       }
     });
+    //editar semestre en el parrafo
+    get_semester_by_lang();
   } catch (error) {
     console.error("Error cargando idioma:", error);
   }
 }
-
-const select = document.getElementById("language-select");
+if (!localStorage.lang) {
+  localStorage.lang = "es";
+}
+const select = document.getElementById("langSelector");
+select.value = localStorage.lang;
+loadLanguage(localStorage.lang);
 select.addEventListener("change", (e) => {
   const lang = e.target.value;
   loadLanguage(lang);
+  localStorage.lang = lang;
 });
-
-// Cargar idioma por defecto
-loadLanguage("es");
