5ee07b8681d4aa8d3f33883e0cedacaaf41a0c85
Lisandro Zapata <zlisandro5@gmail.com>
2025-06-12 16:32:12 -0500

    Se agrega una popup para mejorar la experiencia de usuario en mobile, para evitar que los scroll dificulten la movilidad

--- assets/muestra_bookendtest/js/fs-contr.js
+++ assets/muestra_bookendtest/js/fs-contr.js
@@ -1,7 +1,9 @@
-button_fs = document.getElementById("fs-controller")
-body = document.body
-icon = document.getElementById("icon-fs")
-state = 0
+let button_fs = document.getElementById("fs-controller")
+let warning_popup = document.getElementsByClassName("popup")[0];
+let body = document.body
+let icon = document.getElementById("icon-fs")
+let state = 0
+
 button_fs.addEventListener("click",()=>{
     if(state==0){
         //El modo pantalla completa está desactivado.
@@ -10,12 +12,18 @@ button_fs.addEventListener("click",()=>{
         console.log(state)
         body.requestFullscreen()
         icon.src="resources/exit_fs.png"
+        document.body.style.overflowY = "scroll";
+        warning_popup.style.zIndex = -2;
+        warning_popup.style.opacity = 0;
     }
     else if(state==1 && document.exitFullscreen){
         //Se desactiva
         state=0
         console.log(state)
         icon.src="resources/open_fs.png"
+        document.body.style.overflowY = "hidden";
+        warning_popup.style.zIndex = 2;
+        warning_popup.style.opacity = 1;
         document.exitFullscreen()
     }
     
